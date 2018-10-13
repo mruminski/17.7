c5360609f44cdc781d227940608867e4fa6b05c3
Mateusz Rumiński <matt.ruminski@protonmail.com>
2018-10-13 13:40:06 +0200

    Add real Google auth to code from task 17.6
    Update views

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports= {
+    'GOOGLE_CLIENT_ID': '990569655441-5i2mpq6iprkaiq2k9i4o3suhvdqk553c.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'AucfYVrtC70iWc109l_eyYKC',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
